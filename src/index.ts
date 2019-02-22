bd246beeeb189ee72f2688232a850be3e388dadd
junho.nam <nowonlyhere@gmail.com>
2019-02-22 17:11:55 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Models, Plugin } from '@rematch/core'
+import produce from 'immer'
+import { combineReducers, ReducersMapObject } from 'redux'
+
+function combineReducersWithImmer(reducers: ReducersMapObject) {
+	const reducersWithImmer = {}
+
+	// reducer must return value because literal don't support immer
+	for (const [key, reducerFn] of Object.entries(reducers)) {
+		reducersWithImmer[key] = (state, payload) => {
+			if (typeof state === 'object') {
+				return produce(state, (draft: Models) => {
+					const next = reducerFn(draft, payload)
+					if (typeof next === 'object') {
+						return next
+					}
+				})
+			} else {
+				return reducerFn(state, payload)
+			}
+		}
+	}
+
+	return combineReducers(reducersWithImmer)
+}
+
+// rematch plugin
+const immerPlugin = (): Plugin => ({
+	config: {
+		redux: {
+			combineReducers: combineReducersWithImmer,
+		},
+	},
+})
+
+export default immerPlugin
